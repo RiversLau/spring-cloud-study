0f8a962e1002c0310fe8afa5e4ccd8c55ec0add5
LiuRivers <zhaoxiang0805@hotmail.com>
2018-11-20 21:59:15 +0800

    zuul

---
+++ eureka-zuul-client/src/main/java/com/zhaoxiang/eurekaZuulClient/TokenFilter.java
@@ -0,0 +1,52 @@
+package com.zhaoxiang.eurekaZuulClient;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * @author: Rivers
+ * @date: 2018/11/20
+ */
+@Component
+public class TokenFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object token = request.getParameter("token");
+        if (token == null) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
